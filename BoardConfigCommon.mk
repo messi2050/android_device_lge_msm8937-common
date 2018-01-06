a524c6e75c074067a233b31e924a811a33c8b354
messi2050 <minoforever216@gmail.com>
2018-01-06 02:27:35 +0200

    msm8937: update target arch

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -30,7 +30,7 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_VARIANT := generic
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
@@ -217,6 +217,9 @@ TARGET_RIL_VARIANT := caf
 
 # Sensors
 USE_SENSOR_MULTI_HAL := true
+
+# System prop
+TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
 
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
