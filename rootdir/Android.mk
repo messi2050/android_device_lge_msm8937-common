5e754d7294f3bbc43d54d17181a4b84fa72806b1
messi2050 <minoforever216@gmail.com>
2018-01-17 03:20:56 +0200

    msm8937: update post-boot script

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -58,14 +58,6 @@ LOCAL_SRC_FILES    := init.qcom.post_boot.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.power.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init.qcom.power.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.sensors.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
